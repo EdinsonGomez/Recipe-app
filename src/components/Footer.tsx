49c950f16bb8c8e9c208e52bbe8f15bf0e8936fd
EdinsonGomez <edinsongomez92@gmail.com>
2023-01-22 15:31:36 -0500

    Modify footer responsive

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,10 +4,10 @@ const Footer = () => {
   return (
     <footer>
       <div className="footer">
-        {/* <div className="footer-content"> */}
+        <div className="footer-content">
           <h3 className="footer-text">Con el Patrocinio de</h3>
           <img src="../assets/Grupo 7610.png" alt="footer-img" />
-        {/* </div> */}
+        </div>
       </div>
     </footer>
   )
